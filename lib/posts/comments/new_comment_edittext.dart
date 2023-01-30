75198aaed9bffd519db68f2ab7ed090cbc6a64c6
Umair Khalid <umair.khalid786@outlook.com>
2023-01-30 20:18:13 +0500

    New comment edittext divider added

--- lib/posts/comments/new_comment_edittext.dart
+++ lib/posts/comments/new_comment_edittext.dart
@@ -12,35 +12,38 @@ class NewCommentEditText extends StatefulWidget {
 class NewCommentEditTextState extends State<NewCommentEditText> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      child: Row(children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: CircleAvatar(
-            backgroundImage:
-                AssetImage("assets/images/user${widget.userId}.jpg"),
-            radius: 20,
-          ),
-        ),
-        Expanded(
-            child: TextField(
-                style: Theme.of(context).textTheme.bodySmall,
-                decoration: const InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 18, vertical: 0),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30))),
-                  hintText: 'Enter a search term',
-                ))),
-        Padding(
+    return Column(children: [
+      const Divider(height: 1),
+      Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(children: [
+          Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Text("Post",
-                style: Theme.of(context)
-                    .textTheme
-                    .button
-                    ?.copyWith(color: Colors.blueAccent.withOpacity(0.9))))
-      ]),
-    );
+            child: CircleAvatar(
+              backgroundImage:
+              AssetImage("assets/images/user${widget.userId}.jpg"),
+              radius: 20,
+            ),
+          ),
+          Expanded(
+              child: TextField(
+                  style: Theme.of(context).textTheme.bodySmall,
+                  decoration: const InputDecoration(
+                    contentPadding:
+                    EdgeInsets.symmetric(horizontal: 18, vertical: 0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30))),
+                    hintText: 'Enter a search term',
+                  ))),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Text("Post",
+                  style: Theme.of(context)
+                      .textTheme
+                      .button
+                      ?.copyWith(color: Colors.blueAccent.withOpacity(0.9))))
+        ]),
+      )
+    ],);
   }
 }
